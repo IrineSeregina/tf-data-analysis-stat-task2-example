62481564ea0f4ea6712ca015ef34662f76913da5
IrineSeregina <73775364+IrineSeregina@users.noreply.github.com>
2023-04-06 22:23:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,8 +10,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = p
-    loc = x.mean()
-    betta = p
     
-    return min((loc - 0.092)/(1-alpha) + 0.092, (loc - 0.092)/(betta) + 0.092), max((loc - 0.092)/(1-alpha) + 0.092, (loc - 0.092)/(betta) + 0.092)
+    left = x.max()
+    right = x.max()/((1-p)**(1/len(x)))
+    return left, right
